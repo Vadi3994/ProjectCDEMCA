aea7e916d927d3e449b7f6380bdc69ade4091291
Vadi3994 <57568956+Vadi3994@users.noreply.github.com>
2019-12-07 19:54:24 +0530

    Django DB Update2

--- DjangoDb/models.py
+++ DjangoDb/models.py
@@ -5,3 +5,14 @@ from django.db import models
 class LocEvent(models.Model):
     propertyname = models.CharField('propertyname',max_length=10)
     Location = models.CharField('location',max_length=20)
+    
+    
+class PropertyDetails(models.Model):
+    PropertyName = models.CharField('PropertyName',max_length=100)
+    Address = models.CharField('Address',max_length=100)
+    Location = models.CharField('Location',max_length=100)
+    PhoneNumber = models.CharField('PhoneNumber',max_length=100)
+    AgentName = models.CharField('AgentName',max_length=100)
+    Email = models.CharField('Email',max_length=100)
+    Availability = models.CharField('Availability',max_length=100)
+    PropertyType = models.CharField('PropertyType',max_length=100)
